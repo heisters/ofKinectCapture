797a34e4a1c61bbc31d4df7165c0ce0bc4dc2eda
Ian Smith-Heisters <i@idiosyncra.tc>
2011-08-01 17:23:12 -0700

    Basic skeleton

---
+++ src/KinectCapture.cpp
@@ -0,0 +1,52 @@
+#include "KinectCapture.h"
+
+//--------------------------------------------------------------
+void KinectCapture::setup(){
+
+}
+
+//--------------------------------------------------------------
+void KinectCapture::update(){
+
+}
+
+//--------------------------------------------------------------
+void KinectCapture::draw(){
+
+}
+
+//--------------------------------------------------------------
+void KinectCapture::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void KinectCapture::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void KinectCapture::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void KinectCapture::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void KinectCapture::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void KinectCapture::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void KinectCapture::windowResized(int w, int h){
+
+}
+
